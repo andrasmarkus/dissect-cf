2b3f5adb6dacc067a4496b96ed7b6e5c230ef35f
Biro Mate <h748675@stud.u-szeged.hu>
2020-01-30 23:46:08 +0100

    Refactor DataCapsule DataCapsule is no longer generic. DataCapsule holds its creator as a source (station) DataCapsule holds its destination (application) DataCapsule can store StorageObjects so they can be send together

--- src/main/java/hu/u_szeged/inf/fog/simulator/iot/DataCapsule.java
+++ src/main/java/hu/u_szeged/inf/fog/simulator/iot/DataCapsule.java
@@ -1,68 +1,78 @@
 package hu.u_szeged.inf.fog.simulator.iot;
 
 import hu.mta.sztaki.lpds.cloud.simulator.io.StorageObject;
+import hu.u_szeged.inf.fog.simulator.application.Application;
 
+import java.util.Collection;
 import java.util.Stack;
 
-public class DataCapsule<T> extends StorageObject {
+public class DataCapsule extends StorageObject {
 
-    private T source;
-    private T destination;
-    private Stack<T> dataFlowPath;
+    private Station source;
+    private Application destination;
+    private Stack<Application> dataFlowPath;
+    private Collection<StorageObject> bulkStorageObject;
 
-
-    public DataCapsule(String myid, T source, T destination) {
+    public DataCapsule(String myid, Station source, Application destination) {
         super(myid);
         this.source = source;
         this.destination = destination;
-        this.dataFlowPath = new Stack<T>();
+        this.dataFlowPath = new Stack<Application>();
     }
 
-    public DataCapsule(String myid, long mysize, boolean vary, T source, T destination) {
+    public DataCapsule(String myid, long mysize, boolean vary, Station source, Application destination) {
         super(myid, mysize, vary);
         this.source = source;
         this.destination = destination;
-        this.dataFlowPath = new Stack<T>();
+        this.dataFlowPath = new Stack<Application>();
     }
 
-    public void addToDataPath(T element) {
+    public void addToDataPath(Application element) {
         this.dataFlowPath.push(element);
     }
 
-    public void removeFromDataPath(T element) {
+    public void removeFromDataPath(Application element) {
         if (dataFlowPath.contains(element)) {
             this.dataFlowPath.remove(element);
         }
     }
 
-    public void updateCurrentNodes(T source, T destination) {
-        setSource(source);
-        setDestination(destination);
-    }
-
-    public T getSource() {
+    public Station getSource() {
         return source;
     }
 
-    public void setSource(T source) {
+    public void setSource(Station source) {
         this.source = source;
     }
 
-    public T getDestination() {
+    public Application getDestination() {
         return destination;
     }
 
-    public void setDestination(T destination) {
+    public void setDestination(Application destination) {
         this.destination = destination;
     }
 
-    public Stack<T> getDataFlowPath() {
+    public Stack<Application> getDataFlowPath() {
         return dataFlowPath;
     }
 
-    public void setDataFlowPath(Stack<T> dataFlowPath) {
+    public void setDataFlowPath(Stack<Application> dataFlowPath) {
         this.dataFlowPath = dataFlowPath;
     }
+
+    public Collection<StorageObject> getBulkStorageObject() {
+        return bulkStorageObject;
+    }
+
+    public void setBulkStorageObject(Collection<StorageObject> bulkStorageObject) {
+        this.bulkStorageObject = bulkStorageObject;
+    }
+
+    public void addToBulkStorageObject(StorageObject storageObject) {
+        this.bulkStorageObject.add(storageObject);
+    }
+
 
     //In case we need an actual tree, maybe an inner class would be handy.
 }
