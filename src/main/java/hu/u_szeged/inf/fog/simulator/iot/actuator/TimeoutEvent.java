7b3ef9be02cab07b64072b39a7c059e9135978f0
Biro Mate <h748675@stud.u-szeged.hu>
2020-10-29 13:19:34 +0100

    Fix timeout event

--- src/main/java/hu/u_szeged/inf/fog/simulator/iot/actuator/TimeoutEvent.java
+++ src/main/java/hu/u_szeged/inf/fog/simulator/iot/actuator/TimeoutEvent.java
@@ -1,7 +1,12 @@
 package hu.u_szeged.inf.fog.simulator.iot.actuator;
 
 import hu.mta.sztaki.lpds.cloud.simulator.Timed;
+import hu.mta.sztaki.lpds.cloud.simulator.io.StorageObject;
 import hu.u_szeged.inf.fog.simulator.iot.Station;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class TimeoutEvent implements ActuatorEvent {
     public static long counter = 0;
@@ -11,6 +16,11 @@ public class TimeoutEvent implements ActuatorEvent {
     @Override
     public void actuate(Station station) {
         unprocessed += station.getFilesize();
+        Collection<StorageObject> contents = station.dn.localRepository.contents();
+        List<StorageObject> list = new ArrayList<StorageObject>(contents);
+        for (StorageObject so : list) {
+            station.dn.localRepository.deregisterObject(so);
+        }
         counter++;
     }
 }
